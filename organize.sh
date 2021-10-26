98bed8d8933e513d8b38f958ad01019897e2dfbc
Mark Klose <markklose17@gmail.com>
2021-10-26 17:52:57 -0400

    Wrote script to update website

--- organize.sh
+++ organize.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 # Shell script for moving site items to correct location
-cp -a /md/site_libs/ site_libs/
-rm -rf /md/site_libs/
+if [ -d "md/site_libs" ]
+then
+  cp -a md/site_libs/ site_libs/
+  rm -rf md/site_libs/
+else
+  echo "site_libs already moved"
+fi
+
 # Move HTML documents to top level
-mv /md/*.html /.
+count=`ls -1 md/*.html 2>/dev/null | wc -l`
+if [ $count != 0 ]
+then 
+  mv md/*.html .
+else
+  echo "no HTML files to move"
+fi
